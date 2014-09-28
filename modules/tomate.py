c1f8869875a12f8961b6cd86be657afad2ab97d5
JoYo <jvictor@memsql.com>
2014-09-28 20:10:23 +0000

    Rotten Tomatoes script
    Summary:
    
    Test Plan: psyduck
    
    Reviewers: adam, alex, anders, ankur, mandrews, marko, nika, nikita, pieguy, rob, kouprin, mikhail, jiajia, francisco, rodrigo, sorawit, linh, rumen, cheng, payut, georgi, yordan, marin, raj, jack, joseph
    
    CC: engineering
    
    Maniphest Tasks:

--- modules/tomate.py
+++ modules/tomate.py
@@ -14,25 +14,37 @@ film = None
 
 def RatingOpinion(rating):
     if rating < 40:
-        return random.choice(["It looks lame.", "I heard it sucks.", "I bet its boring.", "Eww."])
+        return random.choice(["It looks lame.", "I heard it sucks.", "I bet its boring.", "Eww.", "LAME!"])
     elif rating < 60:
         return random.choice(["It looks ok-ish.", "I heard its boring.", "Looks mediocre.", "Seriously?"])
     elif rating < 80:
-        return random.choice(["It looks pretty good.", "Sounds like fun.", "Laugh a minute!", "I'd see that."])
-    return random.choice(["It looks awesome!", "Its gonna blow your mine!", "Best movie ever.", "Lets go!"])
+        return random.choice(["It looks pretty good.", "Sounds like fun.", "Laugh a minute!", "I'd see that.", "You'll poop your pants"])
+    return random.choice(["It looks awesome!", "Its gonna blow your mind!", "Best movie ever.", "Lets go!", "awesome tastic!"])
 
+
+additional_words = ["who","actor","star","about","good","like"]
 
 def Handle(query):
     global film
     if "theater" in query or "movie" in query:
         film = RandomFilmInTheaters()
-        furby.say("Go see %s. %s" % (film['title'], RatingOpinion(film["ratings"]["audience_score"])))
+        furby.say("Go see %s." % film['title'])
         query = furby.listen_for(["who","actor","star"], timeout=60.0)
         Handle(query)
     elif film and "who" in query or "actor" in query or "star" in query:
-        furby.say("It's starring %s.  %s." % (random.choice(film["abridged_cast"])["name"], 
-                                              random.choice(["What a hottie","I don't like them", "I love them", "What a hearthrob", "I'd see that", 
-                                                             "LAME", "I'd tap that"])))
+        actor = random.choice(film["abridged_cast"])
+        furby.say("It's starring %s as %s.  %s." % (actor["name"], actor["characters"][0],
+                                                    random.choice(["What a hottie","I don't like them", "I love them", "What a hearthrob", "I'd see that", 
+                                                                   "LAME", "I'd tap that"])))
+    elif film and "about":
+        syn = film['synopsis']
+        ix = syn.index('(C)')
+        if ix != -1:
+            furby.say(syn[:ix])
+        else:
+            furby.say(syn)
+    elif film and "good":
+        furby.say("%s" % RatingOpinion(film["ratings"]["audience_score"]))
 
 if __name__ == "__main__":
     query = furby.get_input()
