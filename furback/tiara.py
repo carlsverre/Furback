086de7218103d58ebe5af9cc113d5add475c0aa0
JoYo <jvictor@memsql.com>
2014-09-28 19:44:49 +0000

    tiara feedback and string issue
    Summary:
    
    Test Plan: psyduck
    
    Reviewers: adam, alex, anders, ankur, mandrews, marko, nika, nikita, pieguy, rob, kouprin, mikhail, jiajia, francisco, rodrigo, sorawit, linh, rumen, cheng, payut, georgi, yordan, marin, raj, jack, joseph
    
    CC: engineering
    
    Maniphest Tasks:

--- furback/tiara.py
+++ furback/tiara.py
@@ -5,5 +5,7 @@ def Insult():
     return subprocess.Popen(["node","/vagrant/plugins/Insult.js"], stdout=subprocess.PIPE).communicate()[0]
 
 def Respond(tweet):
-    os.system("echo '%s' >> ./plugins/tweets.txt" % tweet)
-    return subprocess.Popen(["node","/vagrant/plugins/Respond.js"], stdout=subprocess.PIPE).communicate()[0]
+    os.system("echo '%s' >> ./plugins/tweets.txt" % tweet.replace("'"," "))
+    res =  subprocess.Popen(["node","/vagrant/plugins/Respond.js"], stdout=subprocess.PIPE).communicate()[0].strip()
+    os.system("echo '%s' >> ./plugins/tweets.txt" % res.replace("'"," "))
+    return res
